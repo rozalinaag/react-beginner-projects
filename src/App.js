7a4739b31eb371e098f752c3d00e2f992ca7021d
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2022-10-18 18:08:07 +0300

    animated modal

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
       <button onClick={() => {setOpen(true)}} className="open-modal-btn">✨ Открыть окно</button>
 
-      {open && <div className="overlay">
+     <div className={`overlay animated ${ open? 'show' : ''}`}>
         <div className="modal">
           <svg onClick={() => {setOpen(false)}} height="200" viewBox="0 0 200 200" width="200">
             <title />
@@ -19,7 +19,7 @@ function App() {
           </svg>
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
         </div>
-      </div>}
+      </div>
     </div>
   );
 }
