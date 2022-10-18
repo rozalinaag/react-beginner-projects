62203c416049f0ec4f512755d9257f0419b596ad
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2022-10-18 17:37:35 +0300

    add counter js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './index.scss';
 
 function App() {
+  let count = 0;
+
+  const onCliclPlus = () => {
+    count++;
+  };
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
+        <h1>{count}</h1>
         <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <button onClick = {onCliclPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );
